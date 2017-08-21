1b5d96c69f341541829f4a3866bca7aed3cf950d
Simon Neutert <simonneutert@Simons-Mac-mini.fritz.box>
2017-08-21 12:05:34 +0200

    benchmarking added :heart:

--- lib/iexfib.ex
+++ lib/iexfib.ex
@@ -8,20 +8,33 @@ defmodule Iexfib do
 
   ## Examples
       iex> Iexfib.fib
-      [1, 2, 3, 5, 7]
+      {:ok, [1, 2, 3, 5, 7]}
 
       iex> Iexfib.fib(6)
-      [1, 2, 3, 5, 7, 12]
+      {:ok, [1, 2, 3, 5, 7, 12]}
+
+      iex> Iexfib.fib(100, true)
+      It took 0 MacroSeconds 0 Seconds 5 MicroSeconds
+      {:ok, [354224848179261915075, 218922995834555169026, 135301852344706746049, ...]}
   """
-  def fib(limit \\ 10) do
-    case limit do
-      0 -> IO.inspect 0
-      1 -> IO.inspect 1
-      2 -> IO.inspect [1, 1]
-      n when n > 2 and n < 100000 -> IO.inspect calcfib(limit)
-      x when x < 0 -> IO.puts "Number must be positive and smaller than 100000"
-      _ -> IO.puts "Impossible operation."
+  def fib(limit \\ 10, benchmark \\ false) do
+    starting = :erlang.timestamp
+    sol = case limit do
+      0 -> IO.inspect {:ok, [0]}
+      1 -> IO.inspect {:ok, [1]}
+      2 -> IO.inspect {:ok, [1, 1]}
+      n when n > 2 and n <= 100000 -> {:ok, calcfib(limit)}
+      x when x < 0 -> {:error, "Number must be positive and smaller than 100000"}
+      _ -> {:error, "Impossible operation."}
     end
+    case benchmark do
+      true ->
+        {macss, secss, micss} = starting
+        {macse, secse, micse} = :erlang.timestamp
+        IO.puts "It took #{macse - macss} MacroSeconds #{secse - secss} Seconds #{micse - micss} MicroSeconds"
+      _ ->
+    end
+    sol
   end
 
   @doc """
@@ -29,14 +42,23 @@ defmodule Iexfib do
 
   ## Examples
       iex> Iexfib.grab_fib
-      55
+      {:ok, 55}
 
       iex> Iexfib.grab_fib(6)
-      8
+      {:ok, 8}
+
+      iex> Iexfib.grab_fib(100, true)
+      It took 0 MacroSeconds 0 Seconds 7 MicroSeconds
+      {:ok, 354224848179261915075}
   """
-  def grab_fib(num \\ 10) do
-    [h|_] = calcfib(num)
-    IO.inspect h
+  def grab_fib(num \\ 10, benchmark \\ false) do
+    case fib(num, benchmark) do
+      {:ok, x} ->
+        [h|_] = x
+        {:ok, h}
+      {:error, x} ->
+        x
+    end
   end
 
   defp calcfib(limit, runs \\ 2, arr \\ [1, 1]) do
