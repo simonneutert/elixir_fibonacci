a0bee365ac9977e572dc1ffa956e2716d962ccc9
Simon Neutert <simonneutert@Simons-Mac-mini.fritz.box>
2017-08-21 10:58:19 +0200

    guarding

--- lib/iexfib.ex
+++ lib/iexfib.ex
@@ -14,7 +14,14 @@ defmodule Iexfib do
       [1, 2, 3, 5, 7, 12]
   """
   def fib(limit \\ 10) do
-    IO.inspect calcfib(limit)
+    case limit do
+      0 -> IO.inspect 0
+      1 -> IO.inspect 1
+      2 -> IO.inspect [1, 1]
+      n when n > 2 and n < 100000 -> IO.inspect calcfib(limit)
+      x when x < 0 -> IO.puts "Number must be positive and smaller than 100000"
+      _ -> IO.puts "Impossible operation."
+    end
   end
 
   @doc """
@@ -32,13 +39,13 @@ defmodule Iexfib do
     IO.inspect h
   end
 
-  defp calcfib(limit, comps \\ 2, arr \\ [1, 1]) do
+  defp calcfib(limit, runs \\ 2, arr \\ [1, 1]) do
     cond do
-      comps == limit -> arr
-      comps != limit ->
+      runs != limit ->
         [head|tail] = arr
         [h|_] = tail
-        calcfib(limit, comps+1, [head+h|arr])
+        calcfib(limit, runs+1, [head+h|arr])
+      runs == limit -> arr
     end
   end
 end
