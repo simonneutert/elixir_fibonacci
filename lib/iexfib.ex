5bb6a7548618ef865eed5aaf41508156c81a83c7
Simon Neutert <simonneutert@Simons-Mac-mini.fritz.box>
2017-08-21 16:22:43 +0200

    doc format

--- lib/iexfib.ex
+++ lib/iexfib.ex
@@ -7,15 +7,15 @@ defmodule Iexfib do
   Fibonacci Calculation with Functional Programming
 
   ## Examples
-      iex> Iexfib.fib
-      {:ok, [1, 2, 3, 5, 7]}
+    iex> Iexfib.fib
+    {:ok, [1, 2, 3, 5, 7]}
 
-      iex> Iexfib.fib(6)
-      {:ok, [1, 2, 3, 5, 7, 12]}
+    iex> Iexfib.fib(6)
+    {:ok, [1, 2, 3, 5, 7, 12]}
 
-      iex> Iexfib.fib(100, true)
-      It took 0 MacroSeconds 0 Seconds 5 MicroSeconds
-      {:ok, [354224848179261915075, 218922995834555169026, 135301852344706746049, ...]}
+    iex> Iexfib.fib(100, true)
+    It took 0 MacroSeconds 0 Seconds 5 MicroSeconds
+    {:ok, [354224848179261915075, 218922995834555169026, 135301852344706746049, ...]}
   """
   def fib(limit \\ 10, benchmark \\ false) do
     starting = :erlang.timestamp
@@ -32,7 +32,7 @@ defmodule Iexfib do
         {macss, secss, micss} = starting
         {macse, secse, micse} = :erlang.timestamp
         IO.puts "It took #{macse - macss} MacroSeconds #{secse - secss} Seconds #{micse - micss} MicroSeconds"
-      _ ->
+      _ -> ""
     end
     sol
   end
@@ -41,15 +41,15 @@ defmodule Iexfib do
   Get the value of the nth number in the Fibonacci sequence
 
   ## Examples
-      iex> Iexfib.grab_fib
-      {:ok, 55}
+    iex> Iexfib.grab_fib
+    {:ok, 55}
 
-      iex> Iexfib.grab_fib(6)
-      {:ok, 8}
+    iex> Iexfib.grab_fib(6)
+    {:ok, 8}
 
-      iex> Iexfib.grab_fib(100, true)
-      It took 0 MacroSeconds 0 Seconds 7 MicroSeconds
-      {:ok, 354224848179261915075}
+    iex> Iexfib.grab_fib(100, true)
+    It took 0 MacroSeconds 0 Seconds 7 MicroSeconds
+    {:ok, 354224848179261915075}
   """
   def grab_fib(num \\ 10, benchmark \\ false) do
     case fib(num, benchmark) do
